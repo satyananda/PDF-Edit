21ab46fb7b9329e70705a2ca162027f934fb7bb0
Rajershkumar Pandidurai <rajeshkumar@Rajesh.local>
2015-05-11 11:28:40 +0530

    Initial Commit

---
+++ PDF-PoC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDF-PoC
+//
+//  Created by Rajeshkumar on 11/05/15.
+//  Copyright (c) 2015 T-Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PDF-PoCTests/PDF_PoCTests.m
@@ -0,0 +1,40 @@
+//
+//  PDF_PoCTests.m
+//  PDF-PoCTests
+//
+//  Created by Rajeshkumar on 11/05/15.
+//  Copyright (c) 2015 T-Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PDF_PoCTests : XCTestCase
+
+@end
+
+@implementation PDF_PoCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
